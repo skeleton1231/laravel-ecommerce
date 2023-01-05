62e961c35f9ede218246af1b689b43eb38ee8598
skeleton1231 <814010079@qq.com>
2023-01-05 22:00:43 +0800

    add admin login check

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-
+use Illuminate\Support\Facades\Auth;
 class AdminController extends Controller
 {
     //
@@ -14,8 +13,18 @@ class AdminController extends Controller
     }
 
     //
-    public function login() {
-        echo $password = Hash::make('123456');die;
+    public function login(Request $request) {
+        if($request->isMethod('post')){
+            $data = $request->all();
+            if(Auth::guard('admin')->attempt([
+                'email'=>$data['email'],
+                'password'=>$data['password'],
+                'status'=>1,])){
+                return redirect('/admin/dashboard');
+            } else {
+                return redirect()->back()->with('error_message', 'Invalid Email or Password');
+            }
+        }
         return view('admin.login');
     }
 }
